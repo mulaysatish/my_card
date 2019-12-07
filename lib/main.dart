92a7bf4706d60dfb28ea35766ac7861dd0ddbbac
Satish  Mulay <satish@Satishs-MacBook-Pro.local>
2019-12-07 22:02:11 +0530

    My card app in Flutter

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: double.infinity,
+                height: 1.0,
+              ),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/me_profile_beard.png'),
+              ),
+              Text(
+                'सतिश मुळे',
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'Solway',
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                width: 150.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                elevation: 2.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    '+97 66 62 35 10',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Solway',
+                        color: Colors.teal.shade900),
+                  ),
+                ),
+              ),
+              Card(
+                elevation: 2.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal),
+                  title: Text(
+                    'mulay.satya@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Solway',
+                        color: Colors.teal.shade900),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
